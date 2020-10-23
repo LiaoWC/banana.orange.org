343655387354752ef16cd1ee9ae9a44a8fbbc537
teabao <abcd2012212@gmail.com>
2020-10-23 10:36:58 +0800

    add sidenav

--- public/js/meetingPage_test.js
+++ public/js/meetingPage_test.js
@@ -1,5 +1,31 @@
 console.log("test")
 
-function damm(x = "it") {
-    console.log("damm " + x);
+let sidenav_status = true;
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("room-list-btn").style.marginLeft = "190px";
+    onResize();
+    console.log('call onResize')
 }
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("room-list-btn").style.marginLeft = "10px";
+    onResize();
+}
+
+function OpenorClose() {
+
+    if (sidenav_status)
+        openNav()
+    else
+        closeNav()
+
+    sidenav_status = !sidenav_status;
+}
+
