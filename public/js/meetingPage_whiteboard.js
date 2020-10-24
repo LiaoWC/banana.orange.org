2dfc80a19661e2a5961e5124371afeda48e54270
teabao <abcd2012212@gmail.com>
2020-10-24 16:38:36 +0800

    window whitboard

--- public/js/meetingPage_whiteboard.js
+++ public/js/meetingPage_whiteboard.js
@@ -37,11 +37,11 @@ $(function () {
 
         let canvas_box = canvas.getBoundingClientRect();
 
-
         x0 -= canvas_box.x;
         x1 -= canvas_box.x;
         y0 -= canvas_box.y;
         y1 -= canvas_box.y;
+
 
         context.beginPath();
         context.moveTo(x0, y0);
@@ -77,9 +77,6 @@ $(function () {
     }
 
     function onMouseDown(e) {
-        let canvas_box = canvas.getBoundingClientRect();
-        //console.log(e)
-        //console.log(document.getElementsByTagName('canvas')[0].getBoundingClientRect())
         drawing = true;
         current.x = e.clientX || e.touches[0].clientX;
         current.y = e.clientY || e.touches[0].clientY;
@@ -126,6 +123,7 @@ $(function () {
 
     // make the canvas fill its parent
     function onResize() {
+        console.log('onResize')
         canvas_style = window.getComputedStyle(canvas)
         canvas.width = canvas_style.width.split('.')[0].replace('px', '')
         canvas.height = canvas_style.height.split('.')[0].replace('px', '')
@@ -156,29 +154,4 @@ $(function () {
     }
     window.clearCanvas = clearCanvas
 
-
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    function show_whiteboard_popup() {
-        modal.style.display = "block";
-    }
-    window.show_whiteboard_popup = show_whiteboard_popup
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
 });
