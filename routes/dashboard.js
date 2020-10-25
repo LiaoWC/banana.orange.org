756c1e7d11d6175f44dd9fddfe9ba832205fc90b
teabao <abcd2012212@gmail.com>
2020-10-25 12:52:54 +0800

    ㄐㄜ

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,35 +8,35 @@ router.get('/', (req, res) => {
 })
 
 router.get('/birthday', (req, res) => {
-    axios.post('https://0a9d3cc2f07d.ngrok.io/response',  {'type': 'birthday','content':'徐育倫'})
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+    axios.post('https://65651a0eda8e.ngrok.io/response', { 'type': 'birthday', 'content': '徐育倫' })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     res.redirect('/dashboard')
 })
 
-router.get('/location',(req,res) =>{
-    axios.post('https://0a9d3cc2f07d.ngrok.io/response',  {'type': 'location','content':'台北'})
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+router.get('/location', (req, res) => {
+    axios.post('https://65651a0eda8e.ngrok.io/response', { 'type': 'location', 'content': '台北' })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     res.redirect('/dashboard')
 })
 
-router.get('/vote',(req,res) =>{
-    axios.post('https://0a9d3cc2f07d.ngrok.io/response',  {'type': 'vote','content':'2020-10-30'})
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+router.get('/vote', (req, res) => {
+    axios.post('https://65651a0eda8e.ngrok.io/response', { 'type': 'vote', 'content': '2020-10-30' })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     res.redirect('/dashboard')
 })
 
